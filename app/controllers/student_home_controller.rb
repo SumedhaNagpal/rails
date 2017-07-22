c9181283a5d9f48613abc0f604830033212d3158
SumedhaNagpal <sumedhanagpal@gmail.com>
2017-07-22 17:34:37 +0530

    made changes in css of student_home

--- app/controllers/student_home_controller.rb
+++ app/controllers/student_home_controller.rb
@@ -23,8 +23,22 @@ class StudentHomeController < ApplicationController
   end
 
   def add_experience
+    @experience=Experience.new
   end
 
   def add_por
+    @por=Por.new
   end 
+
+  def show_skills
+    @skills=Skill.where(:student_id => current_student.id)
+  end
+
+  def show_experiences
+    @experiences=Experience.where(:student_id => current_student.id)
+  end
+
+  def show_pors
+    @pors=Por.where(:student_id => current_student.id)
+  end
 end

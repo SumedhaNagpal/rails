61e34af09977a08576ef11cb9d76144ace373071
tanyaa-singh <tanyasingh1996@gmail.com>
2017-07-24 22:54:21 +0530

    Added background gif in devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-	protected
-	def after_sign_in_path_for(resource)
-	student_home_feed_path
-	end
+	
 	private
 
   def sign_up_params
